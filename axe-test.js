9598024cec501b3772c6aebce14f3f5fcdd8297c
Joe Zhou <joe.zhou@cpanel.net>
2017-09-03 09:36:02 -0500

    refactor some code into functions

--- axe-test.js
+++ axe-test.js
@@ -25,19 +25,34 @@ driver.get(configs.server).then(function() {
 driver.sleep(1000);
 
 driver.getCurrentUrl().then(function(url) {
-  var page = url;
+  var baseURL = getBaseURL(url);
 
-  // splice out session token and add page url
+  var page = baseURL;
   if ( configs.page.length > 0 ) {
-    var pots = url.split("/");
-    page = pots[0].concat("//", pots[2], "/", pots[3], configs.page);
+    page = page.concat(configs.page);
   }
 
+  scanURL(page);
+});
+
+driver.quit();
+
+/**
+ * Gets the content of a page and do testing
+ *
+ * @param {String} page url
+ * @return nothing
+ * @api private
+ */ 
+
+function scanURL (url) {
   driver
-    .get(page)
+    .get(url)
     .then(function () {
+      // driver.sleep(10000);
       AxeBuilder(driver)
         .analyze(function(results) {
+          //driver.sleep(5000);
           Object.keys(results).forEach(function(statusKey) {
             if (Array.isArray(results[statusKey])) {
               results[statusKey].forEach(function(statusItem) {
@@ -53,6 +68,23 @@ driver.getCurrentUrl().then(function(url) {
           console.log(JSON.stringify(results, null, '    '));
         });
     });
-});
+}
 
-driver.quit();
+/**
+ * Gets the url with security token if there is one
+ * after login
+ *
+ * @param {String} initial url after login
+ * @return {String} base url
+ * @api private
+ */ 
+
+function getBaseURL (url) {
+  var pots = url.split("/");
+  var base = pots[0].concat("//", pots[2]);
+  var pattern = /^cpsess\d+/;
+  if (pattern.test(pots[3])) {
+    base = base.concat("/", pots[3]);
+  }
+  return base;
+}
